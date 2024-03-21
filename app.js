c0b80fdbabc8293445349d52e16334a98a8023ab
Matteo Chiroli <chiroli.matteo@iisgalvanimi.edu.it>
2024-03-21 16:10:44 +0000

    punto 4

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const pug = require('pug');
 
 const app = express();
 
-// Definisci la rotta per restituire il file JSON
-app.get('/api/album-songs', (req, res) => {
-  // Percorso del file JSON
+// Funzione per ottenere i dati di una canzone in base all'ID
+function getSongById(songId) {
   const filePath = path.join(__dirname, 'songs.json');
+  const rawData = fs.readFileSync(filePath);
+  const songs = JSON.parse(rawData).songs;
+  return songs.find(song => song.song_id == songId);
+}
 
-  // Leggi il file JSON e invialo come risposta
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Errore nella lettura del file JSON:', err);
-      res.status(500).send('Errore interno del server');
-      return;
-    }
+// Rotta per visualizzare i dettagli della canzone
+app.get('/song/:songId', (req, res) => {
+  const songId = req.params.songId;
+  const songData = getSongById(songId);
+  if (!songData) {
+    res.status(404).send('Canzone non trovata');
+    return;
+  }
 
-    // Invia il contenuto del file JSON come risposta
-    res.json(JSON.parse(data));
-  });
+  // Renderizza il template Pug e invialo come risposta
+  const filePath = path.join(__dirname, 'views', 'song.pug');
+  const compiledFunction = pug.compileFile(filePath);
+  const html = compiledFunction({ songData });
+  res.send(html);
 });
 
 // Avvia il server sulla porta 3000
